8d3378f757f05cde6655fda6a710f0c0a97b1739
asankagamage98 <asankagamage1998@gmail.com>
2024-03-24 09:46:59 +0530

    add comment for the route page

--- src/routes/note.router.js
+++ src/routes/note.router.js
@@ -1,11 +1,15 @@
+// Import the controller functions 
 const { getAll, create, update, remove, getById } = require('../controllers/note.controller')
 
+// Import the Express Router
 const router = require('express').Router()
 
-router.get('/', getAll)
-router.get('/:id', getById)
-router.post('/', create)
-router.put('/:id', update)
-router.delete('/:id', remove)
+// Define routes 
+router.get('/', getAll)           // retrieving all notes
+router.get('/:id', getById)       // retrieving a single note by ID
+router.post('/', create)          // creating a new note
+router.put('/:id', update)        // updating an existing note by ID
+router.delete('/:id', remove)     // deleting a note by ID
+
 
 module.exports = router
